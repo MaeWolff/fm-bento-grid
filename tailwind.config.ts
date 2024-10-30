aa1460507fe8aba9c3b212d03c59abf677203194
MaeWolff <maxencewolff.pro@gmail.com>
2024-10-30 12:48:21 +0100

    feat: add custom font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,9 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    fontFamily: {
+      primary: "var(--dm-sans-font)",
+    },
     colors: {
       primary: {
         500: "#7551DC",
