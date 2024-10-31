43ee1d63f295d931bcee59a1eada97b8f7ec9982
MaeWolff <maxencewolff.pro@gmail.com>
2024-10-31 15:29:34 +0100

    feat: add footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,23 +6,46 @@ import ManageMultipleSection from "@/components/home/manage-multiple-section";
 import ScheduleSection from "@/components/home/schedule-section";
 import SocialMediaSection from "@/components/home/social-media-section";
 import WriteAiSection from "@/components/home/write-ai-section";
+import {
+  FRONTEND_MENTOR_PROFILE_URL,
+  PORTFOLIO_URL,
+} from "@/utils/external-links";
 
 export default function Home() {
   return (
-    <main className="md:grid-rows-bento-grid-tablet lg:grid-rows-bento-grid-desktop grid grid-cols-1 gap-8 px-4 py-8 md:grid-cols-2 md:px-10 md:py-14 lg:mx-auto lg:max-w-[1200px] lg:grid-cols-[repeat(4,256px)]">
-      <SocialMediaSection gridPosition="md:col-span-2 lg:col-start-2" />
-      <ManageMultipleSection gridPosition="lg:col-start-2 lg:row-span-2 lg:row-start-2" />
+    <>
+      <main className="md:grid-rows-bento-grid-tablet lg:grid-rows-bento-grid-desktop grid grid-cols-1 gap-8 px-4 py-8 md:grid-cols-2 md:px-10 md:py-14 lg:mx-auto lg:max-w-[1200px] lg:grid-cols-[repeat(4,256px)]">
+        <SocialMediaSection gridPosition="md:col-span-2 lg:col-start-2" />
+        <ManageMultipleSection gridPosition="lg:col-start-2 lg:row-span-2 lg:row-start-2" />
 
-      <ConstistentSection gridPosition="lg:row-span-2" />
+        <ConstistentSection gridPosition="lg:row-span-2" />
 
-      <ScheduleSection gridPosition="md:col-span-2 lg:col-start-4 lg:row-span-3 lg:row-start-1" />
+        <ScheduleSection gridPosition="md:col-span-2 lg:col-span-1 lg:col-start-4 lg:row-span-3 lg:row-start-1" />
 
-      <GrowFollowersSection gridPosition="md:col-span-2 lg:col-start-3 lg:row-span-2" />
-      <FasterGrowthSection gridPosition="md:col-span-2 lg:col-span-1 lg:col-start-2 lg:row-span-2 lg:row-start-4" />
+        <GrowFollowersSection gridPosition="md:col-span-2 lg:col-start-3 lg:row-span-2" />
+        <FasterGrowthSection gridPosition="md:col-span-2 lg:col-span-1 lg:col-start-2 lg:row-span-2 lg:row-start-4" />
 
-      <CreatePostSection gridPosition="lg:col-start-1 lg:row-span-2 lg:row-start-1" />
+        <CreatePostSection gridPosition="lg:col-start-1 lg:row-span-2 lg:row-start-1" />
 
-      <WriteAiSection gridPosition="lg:col-start-1 lg:row-span-3 lg:row-start-3" />
-    </main>
+        <WriteAiSection gridPosition="lg:col-start-1 lg:row-span-3 lg:row-start-3" />
+      </main>
+
+      <footer className="pb-4 text-center text-xs">
+        <p>
+          Challenge from{" "}
+          <a
+            href={FRONTEND_MENTOR_PROFILE_URL}
+            target="_blank"
+            className="text-primary-500"
+          >
+            Frontend Mentor
+          </a>
+          , by{" "}
+          <a href={PORTFOLIO_URL} target="_blank" className="text-primary-500">
+            Maxence WOLFF
+          </a>
+        </p>
+      </footer>
+    </>
   );
 }
---
+++ src/utils/external-links.ts
@@ -0,0 +1,4 @@
+export const FRONTEND_MENTOR_PROFILE_URL =
+  "https://www.frontendmentor.io/profile/MaeWolff";
+
+export const PORTFOLIO_URL = "https://www.maxencewolff.com/";
